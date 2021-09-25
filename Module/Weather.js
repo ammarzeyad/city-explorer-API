a45d383fde63e49990cf2af473c489235b371f01
ammarzeyad <ammarramadan16@yahoo.com>
2021-09-25 18:08:19 +0300

    final

---
+++ Module/Weather.js
@@ -0,0 +1,28 @@
+const axios = require("axios");
+
+function handleWeather(request, response) {
+
+    try{
+    let { city} = request.query;
+    const url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&key=${process.env.WEATHER_API_KEY}`;
+
+    axios.get(url).then(results => {
+        const WeatherArray = results.data.data.map(weather => new Weather(weather));
+        response.status(200).send(WeatherArray);
+    })
+    } catch(error){
+       console.log('something happend', error);
+}
+
+}
+
+
+class Weather{
+    constructor(weather){
+        this.date = weather.datetime;
+        this.description = weather.weather.description;
+        
+    }
+}
+
+module.exports = handleWeather;
--- server.js
+++ server.js
@@ -6,47 +6,28 @@ require('dotenv').config();
 
 const cors =require('cors');
 
+const handleMovie = require('./Module/Movie');
+
+const handleWeather = require('./Module/Weather');
+
 const PORT = process.env.PORT;
 
 const server = express();
-
 server.use(cors());
-
-const weather = require('./data/weather.json');
-
 server.get('/', (request,response) => {
-    response.status(200).send('Hello this is the home page');
+    response.status(200).send('Welcome to the home Route');
 })
-server.listen(PORT,()=> {
-    console.log(`Listening on PORT ${PORT}`);
-})
+// localhost:3009/movie?searchQuery=
+server.get('/movie', handleMovie);
 
-// localhost:3004/weather?searchQ=
-
-server.get('/weather',(request,response)=>{
-    let searchQ = request.query.searchQ;
-    let Ndata = weather.find(value => {
-        if (value.city_name.toLocaleLowerCase() === searchQ.toLocaleLowerCase()) {
-            return value;
-        }
-    })
-    let NewArray = Ndata.data.map(Value => {
-        return new ForeCast(Value.valid_date, Value.weather.description);
-    })
-    response.send(NewArray);
-});
-
-class ForeCast{
-    constructor(a, b){
-        this.date = a;
-        this.description = b;
-    }
-}
+// localhost:3009/weather?city=
+server.get('/weather', handleWeather);
 
 server.get('*', (request, response) =>{
     response.status(404).send('NOT FOUND')
 });
-// function Error(error, request, response) {
-//     response.status(500).send({'Status': 500,responseText:'sorry something went wrong'});
 
-//   };
+server.listen(PORT,()=> {
+    console.log(`Listening on PORT ${PORT}`);
+})
+
